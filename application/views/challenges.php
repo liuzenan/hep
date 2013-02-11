356141661dbe9ffe35bd5651e29978b3fcd49ddc
Sha <wangshasg@gmail.com>
2013-02-11 23:20:53 +0800

    rename challenge tab

--- application/views/challenges.php
+++ application/views/challenges.php
@@ -3,9 +3,9 @@
 	<div class="span10">
 		<div class="row-fluid control-bottons">
 				<ul class="nav nav-tabs">
-					<li class="<?php if($tab=="my") echo "active" ?>"><a href="<?php echo base_url() . "challenges" ?>">My Challenges</a></li>
-					<li class="<?php if($tab=="all") echo "active" ?>"><a href="<?php echo base_url() . "challenges/all" ?>">All Challenges</a></li>
-					<li class="<?php if($tab=="completed") echo "active" ?>"><a href="<?php echo base_url() . "challenges/completed" ?>">Completed Challenges</a></li>
+					<li class="<?php if($tab=="my") echo "active" ?>"><a href="<?php echo base_url() . "challenges" ?>">Current</a></li>
+					<li class="<?php if($tab=="all") echo "active" ?>"><a href="<?php echo base_url() . "challenges/all" ?>">All</a></li>
+					<li class="<?php if($tab=="completed") echo "active" ?>"><a href="<?php echo base_url() . "challenges/completed" ?>">Completed</a></li>
 				</ul>
 		</div>
 		<div id="challenges">
