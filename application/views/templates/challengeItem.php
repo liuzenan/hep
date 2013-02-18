8462f3eb91a4210e8b1abf92e4215fcc4bef8047
liuzenan <liuzenan711@gmail.com>
2013-02-18 22:22:18 +0800

    Add

--- application/views/templates/challengeItem.php
+++ application/views/templates/challengeItem.php
@@ -6,9 +6,5 @@
 			<h4><i class="icon-time icon-large"></i>&nbsp;<?php echo date('H:i',strtotime($start_time)) ?>-<?php echo date('H:i', strtotime($end_time)) ?></h4>	
 		<?php endif ?>
 		<p><?php echo $description ?></p>
-		<div class="challengeControlBtns">
-			<button class="btn joinChallengeNow" data-challenge-id="<?php echo $id ?>" data-user-id="<?php echo $user_id ?>" <?php if($disabled_today) echo "disabled" ?>>Join Today</button>
-			<button class="btn joinChallengeTomorrow" data-challenge-id="<?php echo $id ?>" data-user-id="<?php echo $user_id ?>" <?php if($disabled_tomorrow) echo "disabled" ?>>Join Tomorrow</button>			
-		</div>
 	</div>
 </li>
