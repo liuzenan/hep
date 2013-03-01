1f9d04b7d685f56f4a58ef1e460f995643b5f65b
liuzenan <liuzenan711@gmail.com>
2013-03-01 22:05:12 +0800

    Add

--- application/views/challenges.php
+++ application/views/challenges.php
@@ -12,12 +12,17 @@
 		<div class="row-fluid control-bottons">
 				<ul class="nav nav-tabs">
 					<li class="<?php if($tab=="completed") echo "active" ?>"><a href="<?php echo base_url() . "challenges/completed" ?>">Completed</a></li>
-
 					<li class="<?php if($tab=="all") echo "active" ?>"><a href="<?php echo base_url() . "challenges/all" ?>">All</a></li>
+					<li class="<?php if($tab=="history") echo "active" ?>"><a href="<?php echo base_url() . "challenges/history" ?>">History</a></li>
 				</ul>
 		</div>
 		<div id="challenges" class="<?php echo $tab ?>">
-			<?php $this->load->view('templates/challenge'); ?>
+			<?php if ($tab=="history"): ?>
+				<?php $this->load->view('templates/historyChallenge'); ?>
+			<?php else: ?>
+				<?php $this->load->view('templates/challenge'); ?>
+			<?php endif ?>
+			
 		</div>
 	</div>
 </div>
