a3441d7097e68c9b3161603dc1db6b78d6821d6e
Sha <wangshasg@gmail.com>
2013-03-01 23:59:28 +0800

    added google analytics

--- application/views/faq.php
+++ application/views/faq.php
@@ -18,23 +18,23 @@
           </li>
             <li><b>Can I change the default challenges?</b> <br>
             Yes, you can click on today and tomorrow's challenges to select new ones. Everyday after midnight, if you don't make any choices about today's challenge, the system will automatically copy previous day's challenge to today. If the challenge quota is exceeded, system will select next level challenge in the same category.
-            <img src="http://hep.d2.comp.nus.edu.sg/assets/img/faq/select_challenge.png" />
+            <br><img width="400" height="200" src="http://hep.d2.comp.nus.edu.sg/assets/img/faq/select_challenge.png" />
           </li>
           <li><b> Where can I find the quota for challenges?</b><br>
             Reflected in <i>Challenges</i> page, under the tab of 'All'
-            <img src="http://hep.d2.comp.nus.edu.sg/assets/img/faq/challenge_quota.png" />
+           <br><img width="400" height="200"src="http://hep.d2.comp.nus.edu.sg/assets/img/faq/challenge_quota.png" />
           </li>
           <li><b>Challenges not updated although the homepage shows "data synced 1 minute ago"</b> <br>
             The "1 minute ago" is the last time we get your data from fitbit. It might be your latest data is note synced from your device to fitbit website. Please make sure your progress is reflected on your fitbit profile page first.
-            <img src="http://hep.d2.comp.nus.edu.sg/assets/img/faq/fitbit_progress.png" />
+            <br><img width="400" height="200"src="http://hep.d2.comp.nus.edu.sg/assets/img/faq/fitbit_progress.png" />
           </li>
            <li><b>Is nap also counted as sleep?</b> <br>
             Nope. In fact, you can check this on your fitbit home page. We use the same data to calculate challenge result.
-            <img src="http://hep.d2.comp.nus.edu.sg/assets/img/faq/fitbit_sleep.png" />
+            <br><img width="400" height="200" src="http://hep.d2.comp.nus.edu.sg/assets/img/faq/fitbit_sleep.png" />
           </li>
            <li><b>How can I change email setting</b> <br>
             This can be done at the profile page.
-            <img src="http://hep.d2.comp.nus.edu.sg/assets/img/faq/hep_profile.png" />
+            <br><img width="400" height="200" src="http://hep.d2.comp.nus.edu.sg/assets/img/faq/hep_profile.png" />
           </li>
           <li><b> If I sync data only once a week, can I still get the challenges in the past?</b> <br>
               Yes, as long as your data is recorded in the device. We can backtrack all past challenges.
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -113,7 +113,18 @@
     });
   }
 
-  </script>
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-38433245-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
   	<div class="container">
       <div class="navbar">
         <div class="navbar-inner">
