35a28e77aa78c74948c18921b6e81c10c82c38d4
liuzenan <liuzenan711@gmail.com>
2013-03-06 21:49:06 +0800

    add

---
+++ application/views/badges.php
@@ -0,0 +1,10 @@
+<div class="row-fluid">
+	<?php $this->load->view('templates/sidebar'); ?>
+	<div class="span10">
+		<div class="row-fluid">
+			<pre>
+				<?php var_dump($badges) ?>
+			</pre>
+		</div>
+	</div>
+</div>
