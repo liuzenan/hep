17696b95d28cc4a1951cb286b0936d4e7ae7ccb4
Sha <wangshasg@gmail.com>
2013-10-13 14:50:18 +0300

    views should be under application/views

--- views/templates/achievementItem.php
+++
@@ -1,7 +0,0 @@
-<li class="span4">
-	<div class="thumbnail">
-		<img alt="" src="<?php echo $badge_pic ?>">
-		<h4><?php echo $name ?></h4>
-		<p><?php echo $description ?></p>
-	</div>
-</li>
--- views/templates/challengeCategory.php
+++
@@ -1,9 +0,0 @@
-<?php 
-foreach ($challenges as $challenge) {
-	if ($challenge->category == $type) {
-		$this->load->view($path, $challenge);
-	}
-}
-
- ?>
-		
