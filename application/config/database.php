0379e5896cf021deec5ce64b111f66775e1f3977
Minqi Chen <cnchenminqi@gmail.com>
2013-12-17 22:13:21 +0800

    Cleanup

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'bitnami';
+$db['default']['password'] = '';
 $db['default']['database'] = 'fitbit';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
