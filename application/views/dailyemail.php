af998b052b80f165bee06311f90ffc8230c70e43
Minqi Chen <cnchenminqi@gmail.com>
2014-01-09 15:26:21 +0800

    remove unreferenced view files

--- application/views/dailyemail.php
+++
@@ -1,9 +0,0 @@
-<div class="row-fluid">
-	<div class="span10">
-		<div class="row-fluid myactivity">
-        <?php $this->load->view('templates/challengeList');?>
-				<?php $this->load->view('templates/todayActivity');?>
-				<?php $this->load->view('templates/achievementList');?>
-		</div>
-	</div>
-</div>
